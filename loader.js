f85eb11c3c7bc856d96ad515f67e0cc2b3fd20d6
underwaterlad <scott.writes@outlook.com>
2025-07-10 18:39:36 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 function loadEntry() {
   const hash = window.location.hash.slice(1);
-  const filename = hash ? `${hash}.md` : 'statorithmicarchitecture.md';
+  const filename = hash ? `${hash}.md` : 'dryrun.md';
   const file = `entries/${filename}`;
 
   console.log("Loading:", file);
