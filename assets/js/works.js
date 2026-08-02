ea416ccf3dca83efe4455efa88be1e0d947f5476
underwaterlad <scott.writes@outlook.com>
2026-08-02 15:41:12 +0100

    JSON

---
+++ assets/js/works.js
@@ -0,0 +1,25 @@
+fetch('/assets/data/works.json')
+    .then(response => response.json())
+    .then(works => {
+
+        const gallery = document.getElementById('works-gallery');
+
+        works.forEach(work => {
+
+            const item = document.createElement('div');
+            item.className = 'work';
+
+            item.innerHTML = `
+                <img src="/assets/img/${work.image}" alt="${work.title}">
+
+                <p class="caption">
+                    <i>${work.title}</i><br>
+                    ${work.year} ${work.medium}
+                </p>
+            `;
+
+            gallery.appendChild(item);
+
+        });
+
+    });
